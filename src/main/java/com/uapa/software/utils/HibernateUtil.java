9486d6a142b11b7b1661447080720828c91f1998
grupounosoftwareuapa <grupounosoftwareuapa@gmail.com>
2025-01-03 12:11:56 -0400

    Adding HibernateUtil config

--- src/main/java/com/uapa/software/utils/HibernateUtil.java
+++ src/main/java/com/uapa/software/utils/HibernateUtil.java
@@ -1,44 +1,48 @@
 package com.uapa.software.utils;
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import com.uapa.software.models.Project;
+import com.uapa.software.models.Rol;
+import com.uapa.software.models.Task;
+import com.uapa.software.models.User;
+
 
 public class HibernateUtil {
 
-    // private static SessionFactory sessionFactory;
-    //
-    // public static SessionFactory getSessionFactory() {
-    // if (sessionFactory == null) {
-    // try {
-    // Configuration configuration = new Configuration();
-    //
-    // // Hibernate settings equivalent to hibernate.cfg.xml
-    // Properties settings = new Properties();
-    // settings.put(AvailableSettings.DRIVER, "org.postgresql.Driver");
-    // settings.put(AvailableSettings.URL,
-    // "jdbc:postgresql://localhost:5432/your_database");
-    // settings.put(AvailableSettings.USER, "your_user");
-    // settings.put(AvailableSettings.PASS, "your_password");
-    // settings.put(AvailableSettings.DIALECT,
-    // "org.hibernate.dialect.PostgreSQLDialect");
-    //
-    // // Hibernate behavior
-    // settings.put(AvailableSettings.SHOW_SQL, "true");
-    // settings.put(AvailableSettings.HBM2DDL_AUTO, "update"); // `validate`,
-    // `create`, `create-drop`, `update`
-    //
-    // configuration.setProperties(settings);
-    //
-    // // Register annotated classes
-    // configuration.addAnnotatedClass(Project.class);
-    // configuration.addAnnotatedClass(Task.class);
-    // configuration.addAnnotatedClass(User.class);
-    // configuration.addAnnotatedClass(Rol.class);
-    //
-    // sessionFactory = configuration.buildSessionFactory();
-    //
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // throw new RuntimeException("There was an issue building the SessionFactory");
-    // }
-    // }
-    // return sessionFactory;
-    // }
+	private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, EnvConfig.get("DB_URL"));
+                settings.put(Environment.USER, EnvConfig.get("DB_USER"));
+                settings.put(Environment.PASS, EnvConfig.get("DB_PASSWORD"));
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.H2Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+
+                // Registra tus clases anotadas
+                configuration.addAnnotatedClass(Project.class);
+                configuration.addAnnotatedClass(Task.class);
+                configuration.addAnnotatedClass(User.class);
+                configuration.addAnnotatedClass(Rol.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("Error creando la SessionFactory para pruebas");
+            }
+        }
+        return sessionFactory;
+    }
 }
